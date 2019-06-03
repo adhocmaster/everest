4d845c1f63b450fed02ac7fe39300224cc614d31
Indra Harijono <indra.harijono@huawei.com>
2019-06-03 11:05:41 -0700

    - Fixed bug on Dockerfile

--- components/analytics/flink-everest/build.sh
+++ components/analytics/flink-everest/build.sh
@@ -14,14 +14,24 @@ FLINK_EVEREST_DIR=.
 PIPELINE_JAR_DIR=$FLINK_EVEREST_DIR/target
 PIPELINE_JAR=$PIPELINE_JAR_DIR/$PROJECT*.jar
 PIPELINE_DIR=pipeline
+EVEREST_SINKS_PYTHONS=../kafka-client-demo
+PIPELINE_SINKS_DIR=$PIPELINE_DIR/sinks
 
 if [ ! -d $PIPELINE_JAR_DIR ]
 then
     echo "***** Error ***** : no directory for the job jar files at $PIPELINE_JAR_DIR"
     echo "Suggestion : run 'mvn clean install' on everest flink analytics first to build"
 fi
-#mvn clean install
+# NOW populate the image with the generated directories and files from  projects
+
+# Flink Everest artifacts (jar files)
+# mvn clean install
 cp $PIPELINE_JAR $PIPELINE_DIR
+
+# Sinks everest artifacts (python3)
+cp $EVEREST_SINKS_PYTHONS/requirements.txt $PIPELINE_SINKS_DIR
+cp $EVEREST_SINKS_PYTHONS/src/*.py $PIPELINE_SINKS_DIR
+
 
 docker build --no-cache -t $IMAGE_NAME .
 docker tag $IMAGE_NAME $DOCKER_ID/$IMAGE_NAME:latest
