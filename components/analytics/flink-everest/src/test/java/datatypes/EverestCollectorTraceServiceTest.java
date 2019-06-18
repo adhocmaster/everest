db41a5b1416a1afe1b13b338543d29adfadf2e83
Indra Harijono <indra.harijono@huawei.com>
2019-06-18 13:25:53 -0700

    - Add Tracing analytics

---
+++ components/analytics/flink-everest/src/test/java/datatypes/EverestCollectorTraceServiceTest.java
@@ -0,0 +1,500 @@
+/*
+ * Copyright 2018-2019 The Everest Authors
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+
+package datatypes;
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* EverestCollectorTraceService Tester. 
+* 
+* @author Indra G Harijono
+* @since <pre>Jun 18, 2019</pre> 
+* @version 1.0 
+*/ 
+public class EverestCollectorTraceServiceTest { 
+
+
+/** 
+* 
+* Method: getProcesses() 
+* 
+*/ 
+@Test
+public void testGetProcesses() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setProcesses(Map<String, EverestCTProcess> processes) 
+* 
+*/ 
+@Test
+public void testSetProcesses() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getSpans() 
+* 
+*/ 
+@Test
+public void testGetSpans() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setSpans(Map<String, EverestCTSpan> spans) 
+* 
+*/ 
+@Test
+public void testSetSpans() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: equals(Object o) 
+* 
+*/ 
+@Test
+public void testEqualsO() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: hashCode() 
+* 
+*/ 
+@Test
+public void testHashCode() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getKey() 
+* 
+*/ 
+@Test
+public void testGetKey() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setKey(String key) 
+* 
+*/ 
+@Test
+public void testSetKey() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getType() 
+* 
+*/ 
+@Test
+public void testGetType() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setType(String type) 
+* 
+*/ 
+@Test
+public void testSetType() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getValue() 
+* 
+*/ 
+@Test
+public void testGetValue() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setValue(String value) 
+* 
+*/ 
+@Test
+public void testSetValue() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getRefType() 
+* 
+*/ 
+@Test
+public void testGetRefType() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setRefType(String refType) 
+* 
+*/ 
+@Test
+public void testSetRefType() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getSpanID() 
+* 
+*/ 
+@Test
+public void testGetSpanID() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setSpanID(String spanID) 
+* 
+*/ 
+@Test
+public void testSetSpanIDSpanID() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getTraceID() 
+* 
+*/ 
+@Test
+public void testGetTraceID() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setTraceID(String traceID) 
+* 
+*/ 
+@Test
+public void testSetTraceIDTraceID() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getFields() 
+* 
+*/ 
+@Test
+public void testGetFields() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setFields(Map<String, List<EverestTag>> fields) 
+* 
+*/ 
+@Test
+public void testSetFields() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getTimestamp() 
+* 
+*/ 
+@Test
+public void testGetTimestamp() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setTimestamp(long timestamp) 
+* 
+*/ 
+@Test
+public void testSetTimestamp() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getDuration() 
+* 
+*/ 
+@Test
+public void testGetDuration() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setDuration(long duration) 
+* 
+*/ 
+@Test
+public void testSetDuration() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getLogs() 
+* 
+*/ 
+@Test
+public void testGetLogs() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setLogs(List<EverestLog> logs) 
+* 
+*/ 
+@Test
+public void testSetLogs() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getOperationName() 
+* 
+*/ 
+@Test
+public void testGetOperationName() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setOperationName(String operationName) 
+* 
+*/ 
+@Test
+public void testSetOperationName() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getProcessID() 
+* 
+*/ 
+@Test
+public void testGetProcessID() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setProcessID(String processID) 
+* 
+*/ 
+@Test
+public void testSetProcessID() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getReferences() 
+* 
+*/ 
+@Test
+public void testGetReferences() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setReferences(List<EverestRef> references) 
+* 
+*/ 
+@Test
+public void testSetReferences() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getStartTime() 
+* 
+*/ 
+@Test
+public void testGetStartTime() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setStartTime(long startTime) 
+* 
+*/ 
+@Test
+public void testSetStartTime() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getWarnings() 
+* 
+*/ 
+@Test
+public void testGetWarnings() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setWarnings(String warnings) 
+* 
+*/ 
+@Test
+public void testSetWarnings() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getTags() 
+* 
+*/ 
+@Test
+public void testGetTags() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setTags(List<EverestTag> tags) 
+* 
+*/ 
+@Test
+public void testSetTagsTags() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getServiceName() 
+* 
+*/ 
+@Test
+public void testGetServiceName() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setServiceName(String serviceName) 
+* 
+*/ 
+@Test
+public void testSetServiceName() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getCtprocess() 
+* 
+*/ 
+@Test
+public void testGetCtprocess() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setCtprocess(Map<String, EverestProcess> ctprocess) 
+* 
+*/ 
+@Test
+public void testSetCtprocess() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getSpan() 
+* 
+*/ 
+@Test
+public void testGetSpan() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setSpan(List<EverestSpan> span) 
+* 
+*/ 
+@Test
+public void testSetSpan() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getCtspan() 
+* 
+*/ 
+@Test
+public void testGetCtspan() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setCtspan(Map<String, EverestSpans> ctspan) 
+* 
+*/ 
+@Test
+public void testSetCtspan() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
